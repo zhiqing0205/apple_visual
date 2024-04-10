3928084bec98abf36d748a6aa868e4ed69174bca
Zhiqing <1775840762@qq.com>
2024-04-11 06:10:19 +0800

    remove some unused pages

--- src/views/index.vue
+++ src/views/index.vue
@@ -82,8 +82,6 @@
 import drawMixin from "../utils/drawMixin";
 import { formatTime } from '../utils/index.js'
 import centerLeft1 from './centerLeft1'
-import centerLeft2 from './centerLeft2'
-import centerRight1 from './centerRight1'
 import centerRight2 from './centerRight2'
 import center from './center'
 import bottomLeft from './bottomLeft'
@@ -109,8 +107,6 @@ export default {
   },
   components: {
     centerLeft1,
-    centerLeft2,
-    centerRight1,
     centerRight2,
     center,
     bottomLeft
