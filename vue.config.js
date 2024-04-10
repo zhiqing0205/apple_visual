2e0bd4ee58c17423a9506afa4f12da6a07c2e109
Zhiqing <1775840762@qq.com>
2024-04-11 04:57:15 +0800

    config cros config

--- vue.config.js
+++ vue.config.js
@@ -8,4 +8,16 @@ module.exports = {
     config.resolve.alias
       .set('_c', resolve('src/components')) // key,value自行定义，比如.set('@@', resolve('src/components'))
   },
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 }
