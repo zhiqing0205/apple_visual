76f11b32752118688a7e2463396f42fe8147c4cc
Zhiqing <1775840762@qq.com>
2024-04-11 06:05:52 +0800

    add vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    apple_production_data: [],
+    apple_capacity_data: [],
+    apple_consumption_data: [],
+    fruit_price_data: [],
+    apple_efficiency_data: [],
   },
   mutations: {
+    setAppleProductionData(state, data) {
+      state.apple_production_data = data
+    },
+    setAppleCapacityData(state, data) {
+      state.apple_capacity_data = data
+    },
+    setAppleConsumptionData(state, data) {
+      state.apple_consumption_data = data
+    },
+    setFruitPriceData(state, data) {
+      state.fruit_price_data = data
+    },
+    setAppleEfficiencyData(state, data) {
+      state.apple_efficiency_data = data
+    }
   },
   actions: {
   },
